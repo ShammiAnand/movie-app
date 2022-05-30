c4ed703b5ea68e229b3bb1463abe54445b498e5b
Shammi Anand <54147419+ShammiAnand@users.noreply.github.com>
2022-05-30 15:22:31 +0530

    added middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers'
+import { act } from 'react-dom/test-utils';
+
+// MIDDLEWARE
+// logger({ dispatch, getState })(next)(action)
+/*
+  if there are a chain of middlewares then next will point to the next middlware in the chain
+ */
+const logger = function ({ dispatch, getState }) {
+  return function (next) {
+    return function (action) {
+      // middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);
+    }
+  }
+}
 
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 // console.log('store', store);
 
 // store.dispatch({
